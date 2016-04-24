b1157316ab16888fabd89ed27238b82f1d4553db
Björn Rochel <bjoern@bjro.de>
2016-04-24 12:59:53 +0200

    Simplify unquoting example

--- _10_unquoting.ex
+++ _10_unquoting.ex
@@ -1,22 +1,7 @@
-iex> sum_expr = quote do a + b end
-???
-
-iex> bind_expr = quote do
-          a=1
-          b=2
-        end
-???
-
-iex> final_expr = quote do
-          bind_expr
-          sum_expr
-        end
-WTF???
-
-iex> final_expr = quote do
-          unquote(bind_expr)
-          unquote(sum_expr)
-        end
+iex> quoted = quote do
+        unquote(a) +
+        unquote(b)
+     end
 ???
 
 iex> Code.eval_quoted(final_expr)
